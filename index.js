3e70dc978b75e5e41ab8abdbf90f86553f07ea7d
dina alaa <dinaalaaahmed@gmail.com>
2020-03-11 01:11:54 +0200

    Sign up route and DB some edits

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+const express=require('express');
+const signUp=require('./routes/signUp');
+const bodyparser=require('body-parser');
+const app=express();
+app.use(bodyparser.json());
+
+app.use(signUp);
+//ES6 promises
+mongoose.Promise=global.Promise;
+
+//connect to db before test run
+
+mongoose.connect('mongodb://localhost/spotify', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+app.listen(process.env.port||3000,function(){
+    console.log('listening for a request');
+});
