9879114b49adaee244cce5de761db61f6387aa80
dina alaa <dinaalaaahmed@gmail.com>
2020-03-11 21:11:15 +0200

    userPrivateEndPoint

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ mongoose.Promise=global.Promise;
 //connect to db before test run
 
 mongoose.connect('mongodb://localhost/spotify', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
-
+app.use(function(error,req,res,next){
+    res.send({error:error.message})
+    
+    });
 app.listen(process.env.port||3000,function(){
     console.log('listening for a request');
 });
--- routes/signUp.js
+++ routes/signUp.js
@@ -5,49 +5,59 @@ const spotifySchema = require('../models/DB');
 const bcrypt = require('bcrypt');
 const joi = require('joi');
 router.post('/signup',(req,res)=>{
+
     const shcema = joi.object().keys({
 
         email: joi.string().trim().email().required() ,
         password: joi.string().required(),
         gender: joi.string().required() ,
         country: joi.string().required() ,
-        birthDate: joi.string().required()
+        birthDate: joi.string().required() ,
+        displayName:joi.string().required()
 
     });
+
     joi.validate(req.body,shcema,(err,result)=>{
+
         if(err){
+
             res.send("error occured");
 
         } else {
-            spotifySchema.user.find({email:req.body.email}).exec().then(user=>{
+
+            spotifySchema.user.findOne({email:req.body.email}).exec().then(user=>{
                 if(user){
+
                     res.send("Mail exists");
+
                 }
                 else{
+
                     bcrypt.hash(req.body.password,10,(err,hash)=>{
                         if(err) {
-                            return res.status(500).json({
-                                error:err
-                            });
+
+                            return res.status(500).json({error:err});
+
                         } else {
+
                             const user=new spotifySchema.user({
                                 _id: mongoose.Types.ObjectId(),
                                 email: req.body.email ,
                                 password: hash,
+                                displayName: req.body.displayName ,
                                 gender: req.body.gender ,
                                 country :req.body.country ,
                                 birthDate:req.body.birthDate
                             });
+                            
                             user
                                 .save()
                                 .then(result =>{
                 
                                     res.status(201).json({
                                         message:'User created'
-                                        
-                               
                                     });
-                                    Console.log(result);
+                                    
                                 })
                                 .catch(err=>{
                                     Console.log(err);
@@ -55,7 +65,10 @@ router.post('/signup',(req,res)=>{
                                         error:err
                                     });
                                 });
+                            res.redirect('/login');
+
                         }
+
                     })
 
                 }
@@ -76,7 +89,20 @@ router.post('/signup',(req,res)=>{
 
 });
 
+router.get('/userprivate',(req,res,next)=>{
+    spotifySchema.user.find({_id:req.body.id}).exec().then(user=>{
+        if(user){
+            res.send(user);
+        }
+        else {
+            res.send('user not found');
+        }        
+}).catch(next);
+})
 
+router.get('/login',(req,res,next)=>{
+    res.send('logged');
+})
 
 
 module.exports = router;
